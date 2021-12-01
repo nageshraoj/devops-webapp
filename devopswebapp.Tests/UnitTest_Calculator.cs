e212f65c12d81ad7693edfd0f3c8db7c4657dd2a
Nageswara Rao <nageshraoj@gmail.com>
2021-12-01 15:05:42 +0530

    Initial commit

--- devopswebapp.Tests/UnitTest_Calculator.cs
+++ devopswebapp.Tests/UnitTest_Calculator.cs
@@ -1,22 +1,29 @@
 using System;
 using Xunit;
 using devopswebapp.Models;
+using System.Runtime.InteropServices;
 
 namespace devopswebapp.Tests
 {
     public class UnitTest_Calculator
     {
-        private double valueOne = 10;        
+        // private readonly Random _random = new Random();
+        // private double valueOne = _random.NextDouble(1, 1000);
+        // private double valueTwo = _random.NextDouble(1, 1000);
+
+
+        private double valueOne = 3;
         private double valueTwo = 5;
-        private double knownTotal = 15;
+        private double knownTotal = 8;
         public UnitTest_Calculator()
-        {}
-        
+        { }
+
         [Fact]
         public void TestCalculator()
         {
             Calculation calc = new Calculation();
             var totalValue = calc.AddTwoValues(valueOne, valueTwo);
+            // double knownTotal = valueOne + valueTwo;
             Assert.Equal(totalValue, knownTotal);
         }
     }
